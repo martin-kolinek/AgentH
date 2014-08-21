2066f013404f7d8c5c252b94d8d196e5baf7cb05
Martin Kolinek <martin.kolinek@gmail.com>
2014-08-21 23:43:03 +0200

    A different sample program

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,23 @@
 import FRP.Helm
+import qualified FRP.Helm.Keyboard as Keyboard
 import qualified FRP.Helm.Window as Window
 
-render :: (Int, Int) -> Element
-render (w, h) = collage w h [move (100, 100) $ filled red $ square 64]
+data State = State { mx :: Double, my :: Double }
+
+step :: (Int, Int) -> State -> State
+step (dx, dy) state = state { mx = realToFrac dx + mx state,
+                              my = realToFrac dy + my state }
+
+render :: (Int, Int) -> State -> Element
+render (w, h) (State { mx = mx, my = my }) =
+  centeredCollage w h [move (mx, my) $ filled white $ square 100]
 
 main :: IO ()
 main = do
-  engine <- startup defaultConfig
-  run engine $ render <~ Window.dimensions engine
+    engine <- startup defaultConfig
+
+    run engine $ render <~ Window.dimensions engine ~~ stepper
+
+  where
+    state = State { mx = 0, my = 0 }
+    stepper = foldp step state Keyboard.arrows
